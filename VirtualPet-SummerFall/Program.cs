3830c6c26a17315118f611c00c6799853a04079d
mikespriggs26 <mikespriggs@msn.com>
2017-09-10 17:46:00 -0400

    First commit

---
+++ VirtualPet-SummerFall/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualPet_SummerFall
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VirtualPet Ranger = new VirtualPet();
+
+
+            {   //begin user interaction
+                Console.WriteLine("Ranger's current health stats are: ");
+                Ranger.DogInfo();
+                Console.WriteLine();//added for readability
+
+                List<string> careOptions = new List<string>();
+                careOptions.Add("1. Feed");
+                careOptions.Add("2. Water");
+                careOptions.Add("3. Give Insulin shot");
+
+                for (int i = 0; i < 10; i++) //loop will print options, read input, and adjust variables accordingly
+                {
+                    foreach (string option in careOptions)
+                    {
+                        Console.WriteLine(option);
+                    }
+
+                    Console.WriteLine("Enter the number for the task you want to perform. Press any other key to quit.");
+                    string userChoice = Console.ReadLine();
+                    if (userChoice == "1")
+                    {
+                        Ranger.FeedDog();   //will adjust stats and
+                        Ranger.DogInfo();   //print updated stats
+                        Console.WriteLine("\n=========================\n");  //divider for improved user readability
+                    }
+                    else if (userChoice == "2")
+                    {
+                        Ranger.WaterDog();
+                        Ranger.DogInfo();
+                        Console.WriteLine("\n=========================\n");
+                    }
+                    else if (userChoice == "3")
+                    {
+                        Ranger.GiveInsulin();
+                        Ranger.DogInfo();
+                        Console.WriteLine("\n=========================\n");
+                    }
+                    else
+                    {
+                        Environment.Exit(0);
+                    }
+                }
+
+            }
+
+        }
+    }
+}
---
+++ VirtualPet-SummerFall/VirtualPet.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualPet_SummerFall
+{
+    class VirtualPet
+    {
+        private int hunger;
+        private int water;
+        private int insulin;
+
+        public VirtualPet()
+        {
+            this.hunger = 30;
+            this.water = 30;
+            this.insulin = 40;
+        }
+
+
+        public int Hunger
+        {
+            get { return this.hunger; }
+            set { this.hunger = value; }
+        }
+
+        public int Waste
+        {
+            get { return this.water; }
+            set { this.water = value; }
+        }
+
+        public int Insulin
+        {
+            get { return this.insulin; }
+            set { this.insulin = value; }
+        }
+        public void DogInfo()
+        {
+            Console.WriteLine("Hunger: " + this.hunger);
+            Console.WriteLine("Water: " + this.water);
+            Console.WriteLine("Insulin: " + this.insulin);
+        }
+
+        public void FeedDog()
+        {
+            hunger += 10;
+            water -= 5;
+            insulin -= 4;
+            Console.WriteLine("\nRanger's new stats are:");
+        }
+        public void WaterDog()
+        {
+            water += 10;
+            hunger -= 4;
+            insulin -= 5;
+            Console.WriteLine("\nRanger's new stats are:");
+        }
+        public void GiveInsulin()
+        {
+            insulin += 10;
+            water -= 5;
+            hunger -= 5;
+            Console.WriteLine("\nRanger's new stats are:");
+        }
+
+    }
+}
